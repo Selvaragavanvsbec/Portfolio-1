88f53ae35380730d8d4bb23b1ef3a42c3f8188ab
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2024-12-28 20:54:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const greetings = ["வணக்கம்", "नमस्ते", "ನಮಸ್ಕಾರ", "ഹലോ", "Bonjour", "హలో", "Hello"];
+    const greetingText = document.getElementById('greeting-text');
+    const greetingContainer = document.getElementById('greeting-container');
+    const portfolioHeader = document.getElementById('portfolio-header');
+  
+    let index = 0;
+  
+    const showGreeting = () => {
+      if (index < greetings.length) {
+        greetingText.textContent = greetings[index];
+        index++;
+        setTimeout(showGreeting, 220);
+      } else {
+        greetingContainer.style.display = 'none';
+        document.querySelectorAll('.hidden').forEach(element => {
+          element.style.display = 'block';
+        });
+      }
+    };
+  
+    showGreeting();
+  });
+  
